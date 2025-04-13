6671bf6b080115868380cacad6a0d9bf99cd1178
onghuiyue <onghy-wm23@student.tarc.edu.my>
2025-04-13 16:36:49 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cgcyg5sxouni.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "ventus0123-employee"
+customregion = "us-east-1"
+
